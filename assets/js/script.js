53a992b9011333bbd7e05f105d368774bccb3227
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-01-27 22:16:26 +0000

    Refactored on click function and started working on secondary API request to fetch weather forecast for city

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,72 +21,88 @@ const searchHistory = JSON.parse(localStorage.getItem('history')) || [];
 loadHistory()
 
 // Apply jQuery event on form submission
-$(document).on('click','.btn', function (event) {
-
-    const button = $(this)
-
-    if(button.attr("data-city")){
-        console.log("grey clicked")
-    } else {
-        console.log("blue clicked")
-    }
-
-    // assign variable to user input
-    const userInput = searchInput.val().trim()
+$(document).on('click', '.btn', function (event) {
 
     // Prevent default form functionality 
     event.preventDefault()
-    
-    // Check if user has entered a city if not show bootstrap modal 
-    if (userInput == '') {
-        
-        // call the popup
-        showPopup("Please enter a city")
 
+    // store which button was clicked in a variable
+    const button = $(this)
+
+    // If the button has a data-city attribute, show it's weather
+    if (button.attr("data-city")) {
+
+        // show weather based on history button clicked
+        showWeather(button.attr("data-city"))
+
+        // If the button clicked doesn't have data-city attribute, fall back search value
     } else {
 
-        // showWeather() function
-        showWeather(userInput)
+        // assign variable to user input
+        const userInput = searchInput.val().trim()
+
+        // Check if user has entered a city if not show bootstrap modal 
+        if (userInput == '') {
+
+            // call the popup
+            showPopup("Please enter a city")
+
+        } else {
+
+            // showWeather() function
+            showWeather(userInput, true)
+        }
     }
-    
+
 })
 
-
 // Function that will display the weather results (takes user input)
-function showWeather(userInput){
+function showWeather(userInput, isNewSearch) {
 
     // Create a query string for the "Geocoding API" to determine the Longitude and Latitude of the city
     // this is needed to fetch accurate search results from the "5 Day / 3 Hour Forecast" API
+    // API endpoint: https://openweathermap.org/api/geocoding-api
     const queryString = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=5&appid=' + apiKey;
-    
+
     // Begin jQuery ajax request
     $.ajax({
         url: queryString,
         method: "GET"
-      }).then(function(response){
+    }).then(function (response) {
 
         // Check if the response is not empty, if it is, show an error message to the user
-        if (response.length !== 0){
-
-            console.log(response)
+        if (response.length !== 0) {
 
             const longitude = response[0].lon
             const latitude = response[0].lat
-            console.log(latitude, longitude)
+
+            // Create a query string for the "5 Day / 3 Hour Forecast" to determine the Longitude and Latitude of the city
+            // API endpoint: https://openweathermap.org/forecast5
+            const weatherQueryUrl = 'http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+
+            // Begin jQuery ajax request
+            $.ajax({ url: weatherQueryUrl })
+                .then(function (weatherResponse) {
+
+                    console.log(weatherResponse)
+                });
 
         } else {
 
-        // Show error message to user if City not found
-            showPopup("City not found. Please try again")
+            // Show error message to user if City not found
+            showPopup(`${userInput} not found. Please try again`)
         }
-      });
+    });
 
-    updateHistory(userInput)
+    // If the search is new, then add it to the history
+    if (isNewSearch) {
+        updateHistory(userInput)
+    }
 
 }
 
 // Function for showing/saving search history
-function updateHistory(userInput){
+function updateHistory(userInput) {
 
     // Add current search into searchHistory array[]
     searchHistory.push(userInput);
@@ -100,22 +116,22 @@ function updateHistory(userInput){
 }
 
 // function to load the history
-function loadHistory(){
+function loadHistory() {
 
     // Clear the historyBox contents before we reload the data
     historyBox.empty()
 
-    if(JSON.parse(localStorage.getItem('history'))){
+    if (JSON.parse(localStorage.getItem('history'))) {
         // Loop through each history item, create a button and append to the historyBox element
-        JSON.parse(localStorage.getItem('history')).forEach(function(element){
-            const button = $("<button>").addClass("btn mt-2").text(element).attr("data-city",element)
+        JSON.parse(localStorage.getItem('history')).forEach(function (element) {
+            const button = $("<button>").addClass("btn mt-2").text(element).attr("data-city", element)
             historyBox.prepend(button)
         })
     }
 }
 
 // function to trigger popup with dynamic messages
-function showPopup(message){
+function showPopup(message) {
     // Set modal title
     popUpTitle.text("Error")
     // Set modal description
