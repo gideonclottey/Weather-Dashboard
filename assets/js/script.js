cd1ce9b708449ea6c5fa1132297ec5173f3d0949
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-01-27 20:42:23 +0000

    Worked on function for loading/saving search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,8 @@ const apiKey = "f08da3f97e0c524e1f401f44d632c947"
 // Assign variable for search history (retrieve existing history or create new empty array[] if history is empty)
 const searchHistory = JSON.parse(localStorage.getItem('history')) || [];
 
+loadHistory()
+
 // Apply jQuery event on form submission
 searchForm.submit(function (event) {
 
@@ -39,7 +41,6 @@ searchForm.submit(function (event) {
         showWeather(userInput)
     }
     
-    console.log(userInput)
 })
 
 
@@ -47,20 +48,38 @@ searchForm.submit(function (event) {
 function showWeather(userInput){
     
     const queryString = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=5&appid=' + apiKey;
-    history(userInput)
+    updateHistory(userInput)
 
-    const button = $("<button>").addClass("btn").text(userInput)
-    historyBox.prepend(button)
-    console.log(queryString)
 }
 
 
 // Function for showing/saving search history
-function history(userInput){
-    // Add the history to local storage
+function updateHistory(userInput){
+
+    // Add current search into searchHistory array[]
     searchHistory.push(userInput);
 
+    // Update the localStorage item 'history' with the latest history
     localStorage.setItem('history', JSON.stringify(searchHistory));
+
+    // Refresh the history results
+    loadHistory()
+
+}
+
+// function to load the history
+function loadHistory(){
+
+    // Clear the historyBox contents before we reload the data
+    historyBox.empty()
+
+    if(JSON.parse(localStorage.getItem('history'))){
+        // Loop through each history item, create a button and append to the historyBox element
+        JSON.parse(localStorage.getItem('history')).forEach(function(element){
+            const button = $("<button>").addClass("btn mt-2").text(element).attr("data-city",element)
+            historyBox.prepend(button)
+        })
+    }
 }
 
 // function to trigger popup with dynamic messages
