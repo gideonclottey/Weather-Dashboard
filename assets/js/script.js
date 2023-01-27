3d5d55c37e76c46ab92fe157cbb5db2078e5db37
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-01-27 18:46:40 +0000

    Experimenting with Bootstrap modal popup (to use to prompt errors to user)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
-// Assign variable to form
+// Assign variable to form #search-form
 const searchForm = $("#search-form")
+
+// Assign variable to input field #search-input
+const searchInput = $("#search-input")
+
+// Assign variables to target Bootstrap modal title & content
+const modalTitle = $("#messageModalLabel")
+const modelContent = $(".modal-body")
+
 searchForm.submit(function(event){
 
     event.preventDefault()
-    console.log("submitted")
+    console.log(searchInput.val().trim())
     
+    modalTitle.text(searchInput.val().trim())
+    modelContent.text(searchInput.val().trim())
+
 })
